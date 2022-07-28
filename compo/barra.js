f660c5e208e77d261a5175ad9d72fcedca71eca1
joymar300 <74611985+joymar300@users.noreply.github.com>
2022-07-28 12:30:43 -0500

    agregar proyecto

---
+++ compo/barra.js
@@ -0,0 +1,29 @@
+Vue.component('barra', {
+    template: //html 
+    `
+   
+        <v-app-bar app color="deep-orange lighten-4" >
+            <v-img src="img/icono.png"
+            max-height="60"
+            max-width="75"
+            
+            ></v-img>
+            
+            <v-toolbar-title class="white--text" >Edusex</v-toolbar-title>
+            
+            <v-spacer></v-spacer>
+            
+            <v-btn text>
+                <v-toolbar-title class="white--text " ></v-toolbar-title>
+            </v-btn>
+            <v-btn text>
+                <v-toolbar-title class="white--text" >Registarte</v-toolbar-title>
+            </v-btn>
+            <v-btn text>
+                <v-toolbar-title class="white--text" >Loguin</v-toolbar-title>
+            </v-btn>
+  
+        </v-app-bar>  
+  
+    `
+});
---
+++ compo/pie.js
@@ -0,0 +1,15 @@
+Vue.component('pie', {
+    template: //html 
+    `
+    <template>
+    <v-footer padless class="deep-orange lighten-4">
+      <v-col
+        class="text-center"
+        cols="12"
+      >
+        {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
+      </v-col>
+    </v-footer>
+  </template>
+    `
+});
